2cf22e7b3741c619b1f10d17c732abaff8ca05e5
wartraz <kr.soegaard@gmail.com>
2015-09-02 11:26:06 +0200

    Moved Player to deck folder

---
+++ Deck/Deck/Player.cs
@@ -0,0 +1,30 @@
+ public class Player
+    {
+        private List<Card> playerCards;
+
+        public string Name { get; set; }
+
+        public void ShowHand()
+        {
+            foreach(Card card in playerCards)
+            {
+                Console.WriteLine(card.printcard());
+            }
+        }
+
+        public int TotalValueOfHand()
+        {
+            int sum = 0;
+            foreach (Card card in playerCards)
+            {
+                sum += card.GetCardValue();
+            }
+            return sum;
+        }
+
+        public void AddCard(Card card)
+        {
+            playerCards.Add(card);
+        }
+
+    }
