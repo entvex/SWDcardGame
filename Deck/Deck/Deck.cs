66d48b01effb8fa8d6b0c25a0ecb178295513e4d
David <entvex@gmail.com>
2015-09-02 11:23:30 +0200

    added card

--- Deck/Deck/Deck.cs
+++ Deck/Deck/Deck.cs
@@ -26,32 +26,4 @@ namespace Deck
             }
         }
     }
-
-    class Game
-    {
-        List<player> players = new List<player>();
-        public bool IsGameStarted { get; private set; }
-
-        public AddPlayer()
-        {
-            throw new NotImplementedException();
-        }
-
-        public StartGame()
-        {
-            Console.WriteLine("Welcome to the awesome card game!");
-            throw new NotImplementedException();
-        }
-
-        public dealCards()
-        {
-            throw new NotImplementedException();
-        }
-
-        private string AnnounceWinner(player winner)
-        {
-            Console.WriteLine("{0} has won the game!!!!", player.Name);
-        }
-    }
-
 }
---
+++ Deck/Deck/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Game
+    {
+        List<player> players = new List<player>();
+        public bool IsGameStarted { get; private set; }
+
+        public AddPlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StartGame()
+        {
+            Console.WriteLine("Welcome to the awesome card game!");
+            throw new NotImplementedException();
+        }
+
+        public dealCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string AnnounceWinner(player winner)
+        {
+            Console.WriteLine("{0} has won the game!!!!", player.Name);
+        }
+    }
+}
